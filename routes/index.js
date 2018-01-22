a4f524f08097588cbaa4cb09696a813d81051f6d
XUENN\blackie.tsai <blackie1019@gmail.com>
2018-01-22 13:37:02 +0800

    Updated

--- routes/index.js
+++ routes/index.js
@@ -259,6 +259,19 @@ router.get('/', function (req, res, next) {
   let marketlineCount = req.query.marketline || 1
   golbalTeamNameTimes = 0
 
+  
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
   res.json(_getCompetition(competitionCount, marketlineCount))
 })
 
